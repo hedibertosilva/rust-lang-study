7eab8aa6b6eb33d2f7298bb9eb283fa8f3aea06d
Hediberto <hed.cavalcante@gmail.com>
2021-03-26 22:42:27 -0300

    ending variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,33 @@
 fn main() {
-    let mut x = 5;
+    let x = 5;
     println!("The value of x is: {}", x);
-    x = 6;
+    let x = "Hi";
     println!("The value of x is: {}", x);
 }
+
+/*
+    In the mutable variable we dont can
+    change the type of value.
+
+    * Wrong way:
+        let mut x = 5;
+        x = "Hi";
+
+    But if we redeclare the variable with let 
+    we can change the type of value.
+
+    * Accept way:
+        let x = 5;
+        let x = "Hi";
+
+    The difference between `let` and `const`:
+
+    * Const:
+        - It's assign on build time. 
+        - It's always immutable.
+
+        const MAX_POINTS: u32 = 100_000;
+
+        - const not cam be defined from result of a funcion call.
+
+*/
